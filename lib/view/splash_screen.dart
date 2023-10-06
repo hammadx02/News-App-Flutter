82dc6b497a13d09508303d208d5ab20efffbe9c2
hammadx02 <hammadaali02@gmail.com>
2023-10-06 16:15:48 +0500

    initial commit

---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SpalshScreen extends StatefulWidget {
+  const SpalshScreen({super.key});
+
+  @override
+  State<SpalshScreen> createState() => _SpalshScreenState();
+}
+
+class _SpalshScreenState extends State<SpalshScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
