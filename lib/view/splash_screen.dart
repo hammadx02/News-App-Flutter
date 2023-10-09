06bac7539d19b264c69314c0112c532afac43519
hammadx02 <hammadaali02@gmail.com>
2023-10-09 10:17:40 +0500

    updated

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+import 'dart:js';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:news_app/view/home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -8,8 +13,58 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  final height = MediaQuery.sizeOf(context as BuildContext).height * 1;
+  final width = MediaQuery.sizeOf(context as BuildContext).width * 1;
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 2),
+      () {
+        Navigator.push(
+          context as BuildContext,
+          MaterialPageRoute(
+            builder: (context) => const HomeScreen(),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      // ignore: avoid_unnecessary_containers
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'images/splash_pic.jpg',
+              fit: BoxFit.cover,
+              height: height * 0.5,
+            ),
+            SizedBox(
+              height: height * 0.04,
+            ),
+            Text(
+              'TOP HEADLINES',
+              style: GoogleFonts.anton(
+                letterSpacing: .6,
+                color: Colors.grey.shade700,
+              ),
+            ),
+            SizedBox(
+              height: height * 0.04,
+            ),
+            const SpinKitChasingDots(
+              color: Colors.blue,
+              size: 40,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
