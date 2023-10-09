f8e2d8c02548de69b70b97f1fb0be0e1d892be4c
hammadx02 <hammadaali02@gmail.com>
2023-10-09 11:18:29 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,6 @@ class MyApp extends StatelessWidget {
     );
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.deepPurple,
