e67503b047c14d3a726cf2fefbda4218cee9a968
hammadx02 <hammadaali02@gmail.com>
2023-10-10 10:26:15 +0500

    updated

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,6 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 import 'package:news_app/models/news_channels_headlines_model.dart';
 import 'package:news_app/view_models/news_view_models.dart';
 
@@ -12,6 +14,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 NewsViewModel newsViewModel = NewsViewModel();
+final format = DateFormat('MM, dd, yyyy');
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
@@ -56,13 +59,115 @@ class _HomeScreenState extends State<HomeScreen> {
                 return ListView.builder(
                   itemCount: snapshot.data!.articles!.length,
                   scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {},
+                  itemBuilder: (context, index) {
+                    DateTime dateTime = DateTime.parse(
+                      snapshot.data!.articles![index].publishedAt.toString(),
+                    );
+                    return SizedBox(
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Container(
+                            height: height * 0.6,
+                            width: width * .9,
+                            padding:
+                                EdgeInsets.symmetric(horizontal: height * .02),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(15),
+                              child: CachedNetworkImage(
+                                imageUrl: snapshot
+                                    .data!.articles![index].urlToImage
+                                    .toString(),
+                                fit: BoxFit.cover,
+                                placeholder: (context, url) => Container(
+                                  child: spinkit2(),
+                                ),
+                                errorWidget: (context, url, error) =>
+                                    const Icon(
+                                  Icons.error_outline_rounded,
+                                  color: Colors.red,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Card(
+                            elevation: 5,
+                            color: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: Container(
+                              alignment: Alignment.bottomCenter,
+                              padding: const EdgeInsets.all(15),
+                              height: height * .22,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    width: width * 0.7,
+                                    child: Text(
+                                      snapshot.data!.articles![index].title
+                                          .toString(),
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: GoogleFonts.poppins(
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.w700,
+                                      ),
+                                    ),
+                                  ),
+                                  const Spacer(),
+                                  Container(
+                                    width: width * 0.7,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          snapshot.data!.articles![index]
+                                              .source!.name
+                                              .toString(),
+                                          maxLines: 2,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: GoogleFonts.poppins(
+                                            fontSize: 13,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                        ),
+                                        Text(
+                                          format.format(dateTime),
+                                          maxLines: 2,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: GoogleFonts.poppins(
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 );
               },
             ),
           ),
         ],
       ),
+    );
+  }
+
+  SpinKitFadingCircle spinkit2() {
+    return const SpinKitFadingCircle(
+      color: Colors.amber,
+      size: 50,
     );
   }
 }
