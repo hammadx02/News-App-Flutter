b9e013be827faccf5abc1128f66ad7421dff0517
hammadx02 <hammadaali02@gmail.com>
2023-10-10 23:38:29 +0500

    updated

--- lib/repository/news_repository.dart
+++ lib/repository/news_repository.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:news_app/models/categories_news_models.dart';
 import 'package:news_app/models/news_channels_headlines_model.dart';
 
 class NewsRepository {
@@ -11,6 +12,17 @@ class NewsRepository {
     if (response.statusCode == 200) {
       final body = jsonDecode(response.body);
       return NewsChannelsHeadlinesModel.fromJson(body);
+    }
+    throw Exception('Error');
+  }
+  Future<CategoriesNewsModel> fetchNewsCategoriesApi(String category) async {
+    String url =
+        'https://newsapi.org/v2/everything?q=${category}&apiKey=35a778b7c85a46418b137cafe2d8bd59' ;
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final body = jsonDecode(response.body);
+      return CategoriesNewsModel.fromJson(body);
     }
     throw Exception('Error');
   }
--- lib/view_models/news_view_models.dart
+++ lib/view_models/news_view_models.dart
@@ -1,3 +1,4 @@
+import 'package:news_app/models/categories_news_models.dart';
 import 'package:news_app/models/news_channels_headlines_model.dart';
 import 'package:news_app/repository/news_repository.dart';
 
@@ -6,6 +7,11 @@ class NewsViewModel {
 
   Future<NewsChannelsHeadlinesModel> fetchNewsChannelsHeadlinesApi(String channelName) async {
     final response = await _rep.fetchNewsChannelsHeadlinesApi(channelName);
+    return response;
+  }
+
+   Future<CategoriesNewsModel> fetchNewsCategoriesApi(String category) async {
+    final response = await _rep.fetchNewsCategoriesApi(category);
     return response;
   }
 }
