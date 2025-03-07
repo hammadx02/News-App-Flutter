bd20a6e57e99a41e982131e9b2dc9f3bba3e99c2
Hammad Ali <hammadaali02@gmail.com>
2025-03-07 21:59:34 +0500

    Format URL strings in NewsRepository for consistency

--- lib/repository/news_repository.dart
+++ lib/repository/news_repository.dart
@@ -4,9 +4,10 @@ import 'package:news_app/models/categories_news_models.dart';
 import 'package:news_app/models/news_channels_headlines_model.dart';
 
 class NewsRepository {
-  Future<NewsChannelsHeadlinesModel> fetchNewsChannelsHeadlinesApi(String channelName) async {
+  Future<NewsChannelsHeadlinesModel> fetchNewsChannelsHeadlinesApi(
+      String channelName) async {
     String url =
-        'https://newsapi.org/v2/top-headlines?sources=${channelName}&apiKey=35a778b7c85a46418b137cafe2d8bd59' ;
+        'https://newsapi.org/v2/top-headlines?sources=${channelName}&apiKey=35a778b7c85a46418b137cafe2d8bd59';
     final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
@@ -15,9 +16,10 @@ class NewsRepository {
     }
     throw Exception('Error');
   }
+
   Future<CategoriesNewsModel> fetchNewsCategoriesApi(String category) async {
     String url =
-        'https://newsapi.org/v2/everything?q=${category}&apiKey=35a778b7c85a46418b137cafe2d8bd59' ;
+        'https://newsapi.org/v2/everything?q=$category&apiKey=35a778b7c85a46418b137cafe2d8bd59';
     final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
